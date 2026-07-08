b8c7a5fb8d1645bb71359e376d6dcf0478595eef
Julian Francis <jgfrancis3@gmail.com>
2026-07-08 09:24:58 -0400

    Oxford

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,1 +1,11 @@
-def oxford_comma(array); end
+def oxford_comma(array)
+  if array.length == 0
+    ""
+  elsif array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    "#{array[0..-2].join(', ')}, and #{array[-1]}"
+  end
+end
